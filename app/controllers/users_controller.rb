073ac2c8e806c0d1fe683b7452948c9af69af56d
CarlosAndresValdesHome <carlos.v.83@hotmail.com>
2018-07-19 21:13:25 -0500

    Commit layaut

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,74 +1,66 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
-    @users = User.all
+      render "indexcompany"
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
+  def employee
+      render "employee"
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
 
-  # GET /users/1/edit
-  def edit
-  end
+  def Login
+          redirect_to :controller => 'users', :action => 'login' 
+   end
+  
+  def login
+      
+   if User.exists?(:email=>params[:txtUser],:id=>params[:txtPassword])
+     
+      
+     render 'employee'
+     
+    else 
+      @mensaje = 'usuario y contraseña incorrecta'
+      @tipo ='error'
+      render 'index'
+     end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
+   end
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    def loginEmpresa      
+      if User.exists?(:email=>params[:txtUser],:id=>params[:txtPassword])
+        user = User.select('id_perfi').where(:email=>params[:txtUser],:id=>params[:txtPassword]).first
+        if user.id_perfi.eql?(1)
+            render 'company', layout: 'homeEmpresa'
+        else
+            render 'employee', layout: 'home'
+        end
+      else 
+          @mensaje = 'usuario y contraseña incorrecta'
+          @tipo ='error'
+          render 'indexcompany'
       end
     end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+          
+ 
+    def indexcompany
+      render "indexcompany"
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:id, :nombre, :apellido, :direccion, :telefono, :email, :id_empresa, :id_perfi, :id_perfil_cargo, :id_nomina)
-    end
+    def home 
+      render 'homes' 
+   end
+
+   def Index
+     render 'index'
+   end
+   
+   def loan
+     render 'loan', layout: 'homeEmpresa'
+   end
+
+  
 end
+
+
