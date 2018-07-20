5bc4e9af36bbab77f92cdd7e24041f33f3e12016
MiguelOlayaRamirez <yamse20@yahoo.es>
2018-07-20 17:27:21 -0500

    Confirmación

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,66 +1,67 @@
 class UsersController < ApplicationController
 
-  def index
-      render "indexcompany"
-  end
+          def index
+               render "indexcompany"
+           end
 
-  def employee
-      render "employee"
-  end
+           def loan
+             render 'loan', layout: 'home'
+            end
+              
+            def employee
+               render "employee"
+            end
 
 
-  def Login
-          redirect_to :controller => 'users', :action => 'login' 
-   end
-  
-  def login
-      
-   if User.exists?(:email=>params[:txtUser],:id=>params[:txtPassword])
-     
-      
-     render 'employee'
-     
-    else 
-      @mensaje = 'usuario y contraseña incorrecta'
-      @tipo ='error'
-      render 'index'
-     end
-
-   end
-
-    def loginEmpresa      
-      if User.exists?(:email=>params[:txtUser],:id=>params[:txtPassword])
-        user = User.select('id_perfi').where(:email=>params[:txtUser],:id=>params[:txtPassword]).first
-        if user.id_perfi.eql?(1)
-            render 'company', layout: 'homeEmpresa'
-        else
-            render 'employee', layout: 'home'
-        end
-      else 
-          @mensaje = 'usuario y contraseña incorrecta'
-          @tipo ='error'
-          render 'indexcompany'
-      end
-    end
+          def Login
+                  redirect_to :controller => 'users', :action => 'login' 
+          end
           
- 
-    def indexcompany
-      render "indexcompany"
-    end
+          def login
+              
+           if User.exists?(:email=>params[:txtUser],:id=>params[:txtPassword])
+            
+              
+            render 'employee'
+            
+            else 
+              @mensaje = 'usuario y contraseña incorrecta'
+              @tipo ='error'
+              render 'index'
+            end
 
-    def home 
-      render 'homes' 
-   end
+          end
 
-   def Index
-     render 'index'
-   end
-   
-   def loan
-     render 'loan', layout: 'homeEmpresa'
-   end
+            def loginEmpresa      
+              if User.exists?(:email=>params[:txtUser],:id=>params[:txtPassword])
+                user = User.select('id_perfi').where(:email=>params[:txtUser],:id=>params[:txtPassword]).first
+                if user.id_perfi.eql?(1)
+                    render 'company', layout: 'homeEmpresa'
+                else
+                    render 'employee', layout: 'home'
+                end
+              else 
+                  @mensaje = 'usuario y contraseña incorrecta'
+                  @tipo ='error'
+                  render 'indexcompany'
+              end
+            end
+                  
+        
+            def indexcompany
+              render "indexcompany"
+            end
 
-  
+            def home 
+              render 'homes' 
+          end
+
+          def Index
+            render 'index'
+          end
+
+        
+
 end
 
 
