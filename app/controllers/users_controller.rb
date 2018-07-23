20bbb664414c27e0e6532de45bb0603a9b32e0b2
CarlosAndresValdesHome <carlos.v.83@hotmail.com>
2018-07-23 10:36:16 -0500

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
           def Login
                   redirect_to :controller => 'users', :action => 'login' 
-          end
+      end
           
           def login
               
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
               
             render 'employee'
             
-            else 
+       else 
               @mensaje = 'usuario y contraseña incorrecta'
               @tipo ='error'
               render 'index'
@@ -32,9 +32,9 @@ class UsersController < ApplicationController
 
           end
 
-            def loginEmpresa      
+ def loginEmpresa      
               if User.exists?(:email=>params[:txtUser],:id=>params[:txtPassword])
-                user = User.select('id_perfi').where(:email=>params[:txtUser],:id=>params[:txtPassword]).first
+     user = User.select('id_perfi').where(:email=>params[:txtUser],:id=>params[:txtPassword]).first
                 if user.id_perfi.eql?(1)
                     render 'company', layout: 'homeEmpresa'
                 else
@@ -50,24 +50,20 @@ class UsersController < ApplicationController
         
             def indexcompany
               render "indexcompany"
-            end
+        end
 
             def home 
               render 'homes' 
           end
 
-          def Index
-            render 'index'
+def Index
+ render 'index'
           end
-<<<<<<< HEAD
+          
           def profile
             render 'profiles', layout: 'homeEmpresa'
-          end
-=======
->>>>>>> 6b57d416a54154cc86dc27cd83229299a5c82b9f
-
-        
-
+         
 end
+    end
 
 
